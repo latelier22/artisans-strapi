97a3be099c62b275dda5d7fe06cb241cd24e6b07
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 17:22:42 +0200

    from dist !

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // index.js
 // export { default as ExampleComponent } from './components/ExampleComponent';
 // export { greet } from './functions/utility';
-import Card from './app/Card';
-import Gallery from './app/component/gallery/Gallery'
+import Card from './dist/Card';
+import Gallery from './dist/component/gallery/Gallery'
 
 export {Card, Gallery}
 
