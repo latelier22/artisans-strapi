64f090902fc0a609a128d9c716d7b130581d6b77
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 17:24:17 +0200

    patch

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-// index.js
-// export { default as ExampleComponent } from './components/ExampleComponent';
-// export { greet } from './functions/utility';
-import Card from './Card';
-import Gallery from './component/gallery/Gallery';
-export { Card, Gallery };
