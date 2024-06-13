77914ef17a50c5037c29f7146eb729228fca3974
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 19:14:56 +0200

    v0.1.11

--- dist/component/fetchFiles.js
+++ dist/component/fetchFiles.js
@@ -1,8 +1,23 @@
+// fetchFiles.js
 import myFetch from "./myFetch";
-async function fetchFiles() {
-  const response = await myFetch("/api/upload/files", 'GET', null, 'files');
+async function fetchFiles(fileType = 'all') {
+  const response = await myFetch("/api/upload/files", 'GET', null, 'json');
   const strapiFiles = response;
-  const files = strapiFiles;
+  let files;
+  if (fileType === 'image') {
+    files = strapiFiles.filter(file => file.mime.startsWith('image/'));
+  } else if (fileType === 'video') {
+    files = strapiFiles.filter(file => file.mime.startsWith('video/'));
+  } else {
+    files = strapiFiles; // No filter for 'all', return everything
+  }
   return files;
 }
 export default fetchFiles;
+export function transformResponse(response) {
+  return response.map(file => ({
+    src: `http://vps.latelier22.fr:1336${file.url}`,
+    width: file.width,
+    height: file.height
+  }));
+}
--- dist/test/page.js
+++ dist/test/page.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Gallery from "../component/gallery/Gallery";
-import photos from "../component/gallery/photos";
-const Test = () => {
+// import photos from "../component/gallery/photos";
+import fetchFiles, { transformResponse } from "../component/fetchFiles";
+async function Test() {
+  const files = await fetchFiles("image");
+  const photos = transformResponse(files);
+  console.log(photos);
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
     className: "w-1/2 mx-auto"
-  }, /*#__PURE__*/React.createElement(Gallery, {
+  }, "Artisans", /*#__PURE__*/React.createElement(Gallery, {
     photos: photos
   })));
-};
+}
 export default Test;
