657099c06daaa326a1486fc315372b3755d545e5
latelier22 <latelier.cyrille@gmail.com>
2024-06-13 08:33:07 +0200

    package npm @latelier22/artisans-strapi

---
+++ app/index.js
@@ -0,0 +1,7 @@
+// index.js
+// export { default as ExampleComponent } from './components/ExampleComponent';
+// export { greet } from './functions/utility';
+import Card from './Card';
+
+export {Card}
+
